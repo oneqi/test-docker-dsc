949f565a4d60c8fcebd7c6e282fc209f4e06f684
oneqi <jassbansal@gmail.com>
2017-06-12 18:35:43 +0100

    plus scripts

---
+++ AudioServiceConfig.ps1
@@ -0,0 +1,16 @@
+﻿configuration AudioServiceConfig
+{ 
+    param([string[]] $computerName) 
+ 
+    node $computerName 
+    {    
+        Service Audio 
+        { 
+            Name        = 'Audiosrv' 
+            StartupType = 'Automatic' 
+            State       = 'Running' 
+        }    
+    } 
+} 
+ 
+AudioServiceConfig -ComputerName "add your client hostname here" 
---
+++ LCMPullMode .ps1
@@ -0,0 +1,26 @@
+﻿configuration LCMPullMode 
+{
+    param 
+    ( 
+        [string]$ComputerName,
+        [string]$GUID
+    ) 
+
+    node $ComputerName
+    {    
+        LocalConfigurationManager 
+        { 
+            ConfigurationID                = $GUID
+            ConfigurationMode              = 'ApplyAndAutocorrect' 
+            RefreshMode                    = 'Pull' 
+            DownloadManagerName            = 'WebDownloadManager' 
+            DownloadManagerCustomData = @{ 
+               ServerUrl = 'http://dsc_iis01_1:8080/PSDSCPullServer.svc' 
+               AllowUnsecureConnection = 'true' } 
+            RebootNodeIfNeeded             = $true 
+        } 
+    } 
+}
+
+# e.g.LCMPullMode -ComputerName DESKTOP-F026CP4 -GUID 18ca85b8-1500-4aca-8f05-fbf61c1f48b0
+LCMPullMode -ComputerName "add your client hostname here" -GUID $guidClient
